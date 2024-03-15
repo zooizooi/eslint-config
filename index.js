e2404d5ca82d60606f1b26c8a1327ae7caef2064
ZOOI <zooii.kut@gmail.com>
2024-03-14 23:55:47 -0300

    :wrench: Change rule for 'no-explicit-any' to 'warn'

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ module.exports = {
         {
             rules: {
                 '@typescript-eslint/no-unused-vars': 'warn',
+                '@typescript-eslint/no-explicit-any': 'warn'
             }
         }
     )
